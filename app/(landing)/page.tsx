747332c11de1713eb09937d7d2b6573d70c2f936
bourichi taha <bourichi.taha@gmail.com>
2024-04-11 16:54:48 +0000

    finished landing page with clerk auth sys

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,21 +1,48 @@
 import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton,  SignUpButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  
+
   return (
-    <div className="flex flex-col max-w-[400px] gap-4 p-10">
-      <Button>test</Button>
-      <Button variant={"primary"}>test</Button>
-      <Button variant={"primaryOutline"}>test</Button>
-      <Button variant={"secondary"}>test</Button>
-      <Button variant={"secondaryOutline"}>test</Button>
-      <Button variant={"danger"}>test</Button>
-      <Button variant={"dangerOutline"}>test</Button>
-      <Button variant={"super"}>test</Button>
-      <Button variant={"superOutline"}>test</Button>
-      <Button variant={"ghost"}>test</Button>
-      <Button variant={"sidebar"}>test</Button>
-      <Button variant={"sidebarOutline"}>test</Button>
+    <div className="flex flex-col max-w-[988px] mx-auto w-full lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0 ">
+        <Image src={'/hero.svg'} fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[400px] text-center">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedIn>
+              <Button size={"lg"} variant={"secondary"} className="w-full" asChild>
+                  <Link href={"/learn"} >
+                    Continue learning
+                  </Link>
+              </Button>
+            </SignedIn>
+            <SignedOut>
+              <SignUpButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn"  >
+                <Button size={"lg"} variant={"secondary"} className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn"  >
+                <Button size={"lg"} variant={"primaryOutline"} className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+          </ClerkLoaded>
+        </div>
+      </div>
+
     </div>
   )
 }
