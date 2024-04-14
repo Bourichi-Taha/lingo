6dd9043efb1a5af25876bc7293bfe89fcb985b02
bourichi taha <bourichi.taha@gmail.com>
2024-04-14 15:20:36 +0100

    created the map and map buttons

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,14 +1,16 @@
 import FeedWrapper from '@/components/main/feed-wrapper'
 import Header from '@/components/main/learn/header'
+import Unit from '@/components/main/learn/unit'
 import UserProgress from '@/components/main/learn/user-progress'
 import StickyWrapper from '@/components/main/sticky-wrapper'
-import { getUserProgress } from '@/database/queries'
+import { getUnits, getUserProgress } from '@/database/queries'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
 const LearnPage =async () => {
   const userProgressPromise = getUserProgress();
-  const [userProgress] = await Promise.all([userProgressPromise]);
+  const unitsPromise = getUnits();
+  const [userProgress,units] = await Promise.all([userProgressPromise,unitsPromise]);
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
@@ -16,6 +18,11 @@ const LearnPage =async () => {
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map(unit=>(
+          <div className="mb-10">
+            <Unit activeLesson={undefined}  activeLessonPercentage={0} description={unit.description} id={unit.id} lessons={unit.lessons} order={unit.order} title={unit.title} />
+          </div>
+        ))}
       </FeedWrapper>
       <StickyWrapper>
         <UserProgress activeCourse={userProgress.activeCourse} hasActiveSubscription={false} hearts={userProgress.hearts} points={userProgress.points} />
---
+++ components/main/learn/unit.tsx
@@ -0,0 +1,35 @@
+import { lessons, units } from "@/database/schema";
+import UnitBanner from "./unit-banner";
+import LessonButton from "./lesson-button";
+
+interface UnitProps {
+    id: number;
+    order: number;
+    title: string;
+    description: string;
+    lessons : (typeof lessons.$inferSelect & {completed:boolean})[];
+    activeLesson: typeof lessons.$inferSelect & {unit:typeof units.$inferSelect} | undefined;
+    activeLessonPercentage: number;
+}
+
+const Unit = (props:UnitProps) => {
+    const {activeLesson,activeLessonPercentage,description,id,lessons,order,title} = props;
+  return (
+    <>
+        <UnitBanner title={title} description={description} />
+        <div className="flex items-center flex-col relative">
+            {
+                lessons.map((lesson,index) => {
+                    const isCurrent = (lesson.id === activeLesson?.id);
+                    const isLocked = !lesson.completed && !isCurrent;
+                    return (
+                        <LessonButton key={lesson.id} id={lesson.id} index={index} totalCount={lessons.length-1} current={isCurrent} locked={isLocked} percentage={activeLessonPercentage}   />
+                    )
+                })
+            }
+        </div>
+    </>
+  )
+}
+
+export default Unit
