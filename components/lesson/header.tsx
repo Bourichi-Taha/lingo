9b05dab4c370b7420f05281a526a9b3ce16c1b88
bourichi taha <bourichi.taha@gmail.com>
2024-04-15 13:04:04 +0100

    created on exit quize model

--- components/lesson/header.tsx
+++ components/lesson/header.tsx
@@ -1,6 +1,7 @@
 import { InfinityIcon, X } from "lucide-react";
 import { Progress } from "../ui/progress";
 import Image from "next/image";
+import { useExitModal } from "@/store/use-exit-modal";
 
 interface HeaderProps {
     hearts:number;
@@ -10,11 +11,11 @@ interface HeaderProps {
 
 const Header = (props:HeaderProps) => {
     const {hasActiveSubscription,hearts,percentage} = props;
-
+    const {open} = useExitModal();
 
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
-        <X onClick={()=>{}} className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
+        <X onClick={open} className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
         <Progress value={percentage} />
         <div className="text-rose-500 flex items-center font-bold">
             <Image src={"/heart.svg"} height={28} width={28} alt="Heart" className="mr-2"/>
